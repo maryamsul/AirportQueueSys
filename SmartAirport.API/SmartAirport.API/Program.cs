1ff5aed5620ea041c6ee2d24fc1101c814610761
Maryam Suleiman <94319710+maryamsul@users.noreply.github.com>
2026-07-28 09:13:06 +0300

    Update Program.cs

--- SmartAirport.API/SmartAirport.API/Program.cs
+++ SmartAirport.API/SmartAirport.API/Program.cs
@@ -8,8 +8,8 @@ using SmartAirport.API.Services;
 using SmartAirport.API.Services.Interfaces;
 
 var builder = WebApplication.CreateBuilder(args);
-DotNetEnv.Env.Load();
 
+DotNetEnv.Env.Load();
 builder.Configuration["Jwt:Key"] =
     Environment.GetEnvironmentVariable("JWT_KEY")
     ?? builder.Configuration["Jwt:Key"];
@@ -17,54 +17,72 @@ builder.Configuration["Jwt:Key"] =
 builder.Configuration["AzureServiceBus:ConnectionString"] =
     Environment.GetEnvironmentVariable("AZURE_SERVICE_BUS_CONNECTION_STRING")
     ?? builder.Configuration["AzureServiceBus:ConnectionString"];
-// Services
+
+
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("FrontendPolicy", policy =>
+    {
+        policy
+            .WithOrigins(
+                "http://localhost:5173",
+                "https://agreeable-glacier-08a6b8900.7.azurestaticapps.net"
+            )
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
 
 // Database
 builder.Services.AddDbContext<AirportDbContext>(options =>
     options.UseSqlServer(
-        builder.Configuration.GetConnectionString("AirportConnection")));
+        builder.Configuration.GetConnectionString("AirportConnection")
+    ));
 
 // Dependency Injection
 builder.Services.AddSingleton<DbConnectionFactory>();
-
 builder.Services.AddScoped<IAuthService, AuthService>();
 builder.Services.AddScoped<AuthService>();
 builder.Services.AddScoped<JwtService>();
 builder.Services.AddScoped<TicketService>();
 builder.Services.AddSingleton<ServiceBusPublisher>();
-
 builder.Services.AddSwaggerGen();
 
-
+// Authentication
 builder.Services
 .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
 .AddJwtBearer(options =>
 {
     options.MapInboundClaims = false;
 
-    options.TokenValidationParameters = new TokenValidationParameters
-    {
-        ValidateIssuer = true,
-        ValidateAudience = true,
-        ValidateLifetime = true,
-        ValidateIssuerSigningKey = true,
+    options.TokenValidationParameters =
+        new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
 
-        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+            ValidIssuer =
+                builder.Configuration["Jwt:Issuer"],
 
-        ValidAudience = builder.Configuration["Jwt:Audience"],
+            ValidAudience =
+                builder.Configuration["Jwt:Audience"],
 
-        RoleClaimType = ClaimTypes.Role,
+            RoleClaimType = ClaimTypes.Role,
 
-        IssuerSigningKey =
-            new SymmetricSecurityKey(
-                Encoding.UTF8.GetBytes(
-                    builder.Configuration["Jwt:Key"]!
-                ))
-    };
+            IssuerSigningKey =
+                new SymmetricSecurityKey(
+                    Encoding.UTF8.GetBytes(
+                        builder.Configuration["Jwt:Key"]!
+                    ))
+        };
 });
+
+
 builder.Services.AddAuthorization();
 
 var app = builder.Build();
@@ -75,11 +93,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors("FrontendPolicy");
 app.UseAuthentication();
 app.UseAuthorization();
-
 app.MapControllers();
-
+// Test database endpoint
 app.MapGet("/test-db", async (AirportDbContext db) =>
 {
     var count = await db.QueueTickets.CountAsync();
@@ -89,5 +107,6 @@ app.MapGet("/test-db", async (AirportDbContext db) =>
         Tickets = count
     });
 });
+
 
 app.Run();
