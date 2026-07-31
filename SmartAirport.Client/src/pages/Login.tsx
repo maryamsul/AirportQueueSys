3a3aafcb901e9eb3db99ff14468eec2a76baf401
Maryam Suleiman <94319710+maryamsul@users.noreply.github.com>
2026-07-31 11:32:40 +0300

    Update Login.tsx

--- SmartAirport.Client/src/pages/Login.tsx
+++ SmartAirport.Client/src/pages/Login.tsx
@@ -19,7 +19,7 @@ function Login() {
         setError("Please enter both email and password.");
         return;
     }
-
+console.log("API URL:", import.meta.env.VITE_API_URL);
     try {
         const response = await fetch(
             `${import.meta.env.VITE_API_URL}/api/Auth/login`,
